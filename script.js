8933a88af2e5a686d3ae54d5d9d23135be663aae
frim-mnero <111920793+frim-mnero@users.noreply.github.com>
2023-08-25 13:18:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ closeNav.addEventListener("click", function () {
      backBody.classList.remove("hiddenOver");
     
   }
+if (window.devicePixelRatio !== 1) {
+  // Костыль для определения иных устройств, с коэффициентом отличным от 1
+  var dpt = window.devicePixelRatio;
+  var widthM = window.screen.width * dpt;
+  var widthH = window.screen.height * dpt;
+  document.write(
+    '<meta name="viewport" content="width=' +
+      widthM +
+      ", height=" +
+      widthH +
+      '">'
+  );
+}
