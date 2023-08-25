8b44318efcea2fe13f5b3cec96fa00846466f6a5
frim-mnero <111920793+frim-mnero@users.noreply.github.com>
2023-08-25 05:29:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 let burger = document.querySelector(".burger");
 let closeNav = document.querySelector(".closeNav");
-let nav = document.querySelector('nav')
-let backBody = document.querySelector('body');
+let nav = document.querySelector("nav");
+let backBody = document.querySelector("body");
 let shadow = document.querySelector(".shadowBlueBlock");
-let zapic = document.querySelectorAll("button.title__zapic");
+let formZapic = document.querySelector(".contBlockCons");
+let closeForm = document.querySelector(".closeWindow");
 
-console.log(zapic);
-document.addEventListener('clicl',function(evt){
-  if(evt.target.dataSet.type==zapic){
-    alert("sss");
-  }
-});
 
 burger.addEventListener("click", function () {
   backBody.classList.add("backgroundBody");
   nav.classList.add("openNav");
-  shadow.classList.add("displBlock");
+  backBody.classList.add("hiddenOver");
 });
-closeNav.addEventListener('click',function(){
+closeNav.addEventListener("click", function () {
   nav.classList.remove("openNav");
   backBody.classList.remove("backgroundBody");
-  shadow.classList.remove("displBlock");
-});
+ backBody.classList.remove("hiddenOver");
+  });
+
+  function hello() {
+   formZapic.classList.add("openFormZapics");
+   shadow.classList.add("displBlock");
+   backBody.classList.add("hiddenOver");
+  }
+  function buy(){
+    formZapic.classList.remove("openFormZapics");
+    shadow.classList.remove("displBlock");
+     backBody.classList.remove("hiddenOver");
+    
+  }
